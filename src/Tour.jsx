c9be9714494c5d7e33d5720585a8c300c98b5deb
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-08 17:18:03 +0330

    show tours!

--- src/Tour.jsx
+++ src/Tour.jsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const Loading = () => {
+const Tour = ({id, image, info, price, name}) => {
+  const [readMore, setReadMore] = useState(false);
   return (
-    <div className="loading">
-      <h1>single</h1>
-    </div>
+    <article className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+        <p>{info}</p>
+        <button className="delete-btn">not interested</button>
+      </footer>
+    </article>
   );
 };
 
-export default Loading;
+export default Tour;
