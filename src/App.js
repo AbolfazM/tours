d620956f73109f46db8edb4140dbd1139b4fd3f4
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-08 16:11:49 +0330

    add codes!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
 const App = () => {
-  return (
-    <div className="App"></div>
-  );
+  return <h2>Tours Project Setup</h2>
 }
 
-export default App;
+export default App
