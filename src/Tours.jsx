ed852b0a99a730d11056a1d352e8faade3f7b9fd
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-08 17:10:12 +0330

    map tour!

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import Tour from './Tour';
+
 const Tours = ({tours}) => {
-  return <h2>tours component</h2>;
+	return(
+		<section>
+			<div className="title">
+				<h2>our tours</h2>
+				<div className="underline"></div>
+			</div>
+			<div>
+				{tours.map((tour) => {
+					return <Tour key={tour.id} {...tour}></Tour>
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Tours;
