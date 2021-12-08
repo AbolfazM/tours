8375c8190b7c54c19e44d7d4a1bc8b229287b9de
AbolfazM <abolfazl.m.2021.1400@gmail.com>
2021-12-08 16:26:05 +0330

    change default!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
 const url = 'https://course-api.com/react-tours-project'
+
 const App = () => {
   return <h2>Tours Project Setup</h2>
 }
